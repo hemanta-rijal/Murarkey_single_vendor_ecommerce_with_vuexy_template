90877985f832459878969b07fa2abbfcd4e11eed
Prem Neupane <bindas.prem.75@gmail.com>
2021-05-17 19:58:46 +0545

    active class added for current route | .gitignore updated

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -16,78 +16,78 @@
             </li>
             <li class=" navigation-header"><span>CMS</span>
             </li>
-            <li class="nav-item {{ request()->is('admin/users*') ? 'active' : '' }}"><a href="#"><i class="feather icon-user"></i>
+            <li class="nav-item {{ request()->is('admin/users*') ? 'active' : '' }} "><a href="#"><i class="feather icon-user"></i>
                 <span class="menu-title" data-i18n="Users">Users</span></a>
                 <ul class="menu-content">
-                    <li><a href="{{ route('admin.users.index') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="View All">View All</span></a>
+                    <li class=" {{\Request::route()->getName()=='admin.users.index' ? 'active' : ''}}"><a href="{{ route('admin.users.index') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="View All">View All</span></a>
                     </li>
-                    <li><a href="{{ route('admin.users.create') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Add New">Add New</span></a>
+                    <li  class="{{\Request::route()->getName()=='admin.users.create' ? 'active' : ''}}"><a href="{{ route('admin.users.create') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Add New">Add New</span></a>
                     </li>
                     {{-- <li><a href="{{ route('admin.users.sellers-trash') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Associate Seller Trash">Associate Seller Trash</span></a>
                     </li> --}}
-                    <li><a href="{{ route('admin.users.trash') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Trash">Trash</span></a>
+                    <li  class="{{\Request::route()->getName()=='admin.users.sellers-trash' ? 'active' : ''}}"><a href="{{ route('admin.users.trash') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Trash">Trash</span></a>
                     </li>
                 </ul>
             </li>
             <li class=" nav-item {{ request()->is('admin/categories*') ? 'active' : '' }}"><a href="#"><i class="feather icon-list"></i>
                 <span class="menu-title" data-i18n="Categories">Categories</span></a>
                 <ul class="menu-content">
-                    <li><a href="{{ route('admin.categories.index') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="View All">View All</span></a>
+                    <li class="{{\Request::route()->getName()=='admin.categories.index' ? 'active' : ''}}"><a href="{{ route('admin.categories.index') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="View All">View All</span></a>
                     </li>
-                    <li><a href="{{ route('admin.categories.create') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Add New">Add New</span></a>
+                    <li class="{{\Request::route()->getName()=='admin.categories.create' ? 'active' : ''}}"><a href="{{ route('admin.categories.create') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Add New">Add New</span></a>
                     </li>
-                    <li><a href="{{ route('admin.categories.order') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Order">Order</span></a>
+                    <li class="{{\Request::route()->getName()=='admin.categories.order' ? 'active' : ''}}"><a href="{{ route('admin.categories.order') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Order">Order</span></a>
                     </li>
-                    <li><a href="{{ route('admin.categories.upload') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Import Form Excel File">Import Form Excel File</span></a>
+                    <li class="{{\Request::route()->getName()=='admin.categories.upload' ? 'active' : ''}}"><a href="{{ route('admin.categories.upload') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Import Form Excel File">Import Form Excel File</span></a>
                     </li>
                 </ul>
             </li>
             <li class=" nav-item {{ request()->is('admin/products*') ? 'active' : '' }}"><a href="#"><i class="feather icon-box"></i>
                 <span class="menu-title" data-i18n="Products">Products</span></a>
                 <ul class="menu-content">
-                    <li><a href="{{ route('admin.products.index') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="View All">View All</span></a>
+                    <li class="{{\Request::route()->getName()=='admin.products.index' ? 'active' : ''}}"><a href="{{ route('admin.products.index') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="View All">View All</span></a>
                     </li>
-                    <li><a href="{{ route('admin.products.create') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Post a New Product">Post a New Product</span></a>
+                    <li class="{{\Request::route()->getName()=='admin.products.create' ? 'active' : ''}}"><a href="{{ route('admin.products.create') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Post a New Product">Post a New Product</span></a>
                     </li>
-                    <li><a href="{{ route('admin.products.index') }}?type=pending"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Pending Products">Pending Products</span></a>
+                    <li class="{{\Request::route()->getName()=='admin.products.update-status' ? 'active' : ''}}"><a href="{{ route('admin.products.index') }}?type=pending"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Pending Products">Pending Products</span></a>
                     </li>
-                    <li><a href="{{ route('admin.products.trash') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Trash Items">Trash Items</span></a>
+                    <li class="{{\Request::route()->getName()=='admin.products.trash' ? 'active' : ''}}"><a href="{{ route('admin.products.trash') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Trash Items">Trash Items</span></a>
                     </li>
                 </ul>
             </li>
             <li class=" nav-item {{ request()->is('admin/flash-sales*') ? 'active' : '' }}"><a href="#"><i class="feather icon-zap"></i>
                 <span class="menu-title" data-i18n="Flash Sale">Flash Sales</span></a>
                 <ul class="menu-content">
-                    <li><a href="{{ route('admin.flash-sales.index') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="View All">View All</span></a>
+                    <li class="{{\Request::route()->getName()=='admin.flash-sale.index' ? 'active' : ''}}"><a href="{{ route('admin.flash-sales.index') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="View All">View All</span></a>
                     </li>
-                    <li><a href="{{ route('admin.flash-sales.create') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Add New">Add New</span></a>
+                    <li class="{{\Request::route()->getName()=='admin.flash-sale.create' ? 'active' : ''}}"><a href="{{ route('admin.flash-sales.create') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Add New">Add New</span></a>
                     </li>
                 </ul>
             </li>
             <li class=" nav-item {{ request()->is('admin/orders*') ? 'active' : '' }}"><a href="#"><i class="feather icon-shopping-cart"></i>
                 <span class="menu-title" data-i18n="Orders">Orders</span></a>
                 <ul class="menu-content">
-                    <li><a href="#"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="View All">View All</span></a>
+                    <li class="{{\Request::route()->getName()=='' ? 'active' : ''}}"><a href="#"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="View All">View All</span></a>
                     </li>
-                    <li><a href="#"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Add New">Add New</span></a>
+                    <li class="{{\Request::route()->getName()==' ' ? 'active' : ''}}"><a href="#"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Add New">Add New</span></a>
                     </li>
                 </ul>
             </li>
             <li class=" nav-item {{ request()->is('admin/reports*') ? 'active' : '' }}"><a href="#"><i class="feather icon-bar-chart-2"></i>
                 <span class="menu-title" data-i18n="Reports">Reports</span></a>
                 <ul class="menu-content">
-                    <li><a href="#"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="View All">View All</span></a>
+                    <li class="{{\Request::route()->getName()=='' ? 'active' : ''}}"><a href="#"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="View All">View All</span></a>
                     </li>
-                    <li><a href="#"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Add New">Add New</span></a>
+                    <li class="{{\Request::route()->getName()=='' ? 'active' : ''}}"><a href="#"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Add New">Add New</span></a>
                     </li>
                 </ul>
             </li>
             <li class=" nav-item {{ request()->is('admin/system-messages*') ? 'active' : '' }}"><a href="#"><i class="feather icon-message-square"></i>
                 <span class="menu-title" data-i18n="System Messages">System Messages</span></a>
                 <ul class="menu-content">
-                    <li><a href="{{ route('admin.system-messages.index') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="View All">View All</span></a>
+                    <li class="{{\Request::route()->getName()=='admin.system-messages.index' ? 'active' : ''}}"><a href="{{ route('admin.system-messages.index') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="View All">View All</span></a>
                     </li>
-                    <li><a href="{{ route('admin.system-messages.create') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Add New">Add New</span></a>
+                    <li class="{{\Request::route()->getName()=='admin.system-messages.create' ? 'active' : ''}}"><a href="{{ route('admin.system-messages.create') }}"><i class="feather icon-circle"></i><span class="menu-item" data-i18n="Add New">Add New</span></a>
                     </li>
                 </ul>
             </li>
