6e764312d304f0312baebcc082616c29d2c568ba
Hemanta Rijal <foreverhemanta221@gmail.com>
2021-06-16 12:22:47 +0545

    some changes

--- app/Http/ApiRequests/LoginRequest.php
+++ app/Http/ApiRequests/LoginRequest.php
@@ -21,5 +21,4 @@ class LoginRequest extends BaseRequest
         ];
     }
 
-
 }
--- routes/admin.php
+++ routes/admin.php
@@ -389,11 +389,11 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
         Route::get('contact-us/update-status/{id}', 'PagesController@contactUsUpdateStatus');
 
         //frontend & system settings
-//        Route::get('/frontend-settings/homepage-setting', function () {return view('admin.settings.home-page-setting');})->name('admin.frontend-settings.homepage-setting');
-//        Route::get('/system-settings/general-setting', function () {return view('admin.settings.general-setting');})->name('admin.system-settings.general-setting');
-//        Route::get('/system-settings/payment-setting', function () {return view('admin.settings.payment-setting');})->name('admin.system-settings.payment-setting');
-//        Route::get('/system-settings/shipping-setting', function () {return view('admin.settings.shipping-method-setting');})->name('admin.system-settings.shipping-setting');
-//        Route::get('/system-settings/social-login-setting', function () {return view('admin.settings.social-login-setting');})->name('admin.system-settings.social-login-setting');
+        Route::get('/frontend-settings/homepage-setting', function () {return view('admin.settings.home-page-setting');})->name('admin.frontend-settings.homepage-setting');
+        Route::get('/system-settings/general-setting', function () {return view('admin.settings.general-setting');})->name('admin.system-settings.general-setting');
+        Route::get('/system-settings/payment-setting', function () {return view('admin.settings.payment-setting');})->name('admin.system-settings.payment-setting');
+        Route::get('/system-settings/shipping-setting', function () {return view('admin.settings.shipping-method-setting');})->name('admin.system-settings.shipping-setting');
+        Route::get('/system-settings/social-login-setting', function () {return view('admin.settings.social-login-setting');})->name('admin.system-settings.social-login-setting');
 
         Route::post('site-settings', 'SiteSettingsController@update')
             ->name('admin.site-settings.update');
