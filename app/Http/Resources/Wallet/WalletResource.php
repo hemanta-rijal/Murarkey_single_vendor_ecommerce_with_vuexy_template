9e60123def0c39ee348f5a718b737a8f1d96bdb6
Prem Neupane <bindas.prem.75@gmail.com>
2021-06-25 17:53:08 +0545

    updates

--- app/Http/Resources/Wallet/WalletResource.php
+++ app/Http/Resources/Wallet/WalletResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Wallet;
 
+use App\Http\Resources\User\UserResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class WalletResource extends JsonResource
@@ -14,6 +15,17 @@ class WalletResource extends JsonResource
      */
     public function toArray($request)
     {
-        return $this->backupdata();
+        // return $this->backupdata();
+        return [
+            'user_id' => new UserResource($this->user_id),
+            'transaction_type' => $this->transaction_type,
+            'payment_method' => $this->payment_method,
+            'description' => $this->description,
+            'amount' => $this->amount,
+            'status' => $this->status,
+            'total_amount' => $this->total_amount,
+
+        ];
+
     }
 }
