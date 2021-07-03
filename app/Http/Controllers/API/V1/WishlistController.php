bcb5c39fbeca5e20338b62e580db7848fc43395d
Prem Neupane <bindas.prem.75@gmail.com>
2021-07-03 14:39:58 +0545

    wallet transactions and

--- app/Http/Controllers/API/V1/WishlistController.php
+++ app/Http/Controllers/API/V1/WishlistController.php
@@ -1,53 +1,64 @@
 <?php
 
-
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\ApiRequests\ApiCartRequest;
 use App\Http\ApiRequests\ApiCartUpdateRequest;
-use App\Modules\Cart\Requests\WishlistExistsRequest;
 use Modules\Cart\Services\WishlistService;
-
 
 class WishlistController extends BaseController
 {
     private $wishlistService;
-
-
+    private $auth;
     public function __construct(WishlistService $wishlistService)
     {
         $this->wishlistService = $wishlistService;
+        $this->auth = auth();
     }
 
     public function index()
     {
-        return $this->wishlistService->getCartByUser($this->auth->user());
+        $data = $this->wishlistService->getWishlistByUser($this->auth->user());
+
+        return response()->json(['data' => $data, 'success' => true, 'message' => 'Items fetched from wishlist', 'status' => 200]);
 
     }
 
     public function store(ApiCartRequest $request)
     {
+        $items = $this->wishlistService->getWishlistByUser($this->auth->user());
+
+        if ($items->filter(function ($item) use ($request) {
+            return $item->id == $request->product_id;
+        })->count() > 0) {
+            return response()->json(['success' => false, 'message' => 'Item already exists in wishlist', 'status' => 500]);
+        }
         $this->wishlistService->add($this->auth->user(), $request->all());
+        return response()->json(['success' => true, 'message' => 'Item added in wishlist', 'status' => 200]);
+
     }
 
     public function destroy($rowId)
     {
         $this->wishlistService->delete($this->auth->user(), $rowId);
+        return response()->json(['success' => true, 'message' => 'Item removed from wishlist', 'status' => 200]);
+
     }
 
     public function update($rowId, ApiCartUpdateRequest $request)
     {
         $this->wishlistService->update($this->auth->user(), $rowId, $request->only('qty', 'options'));
+        return response()->json(['success' => true, 'message' => 'Item updated on wishlist', 'status' => 200]);
 
     }
 
-    public function productExists(WishlistExistsRequest $request)
+    public function productExists(Request $request)
     {
-        $items = $this->wishlistService->getCartByUser($this->auth->user());
+        $items = $this->wishlistService->getWishlistByUser($this->auth->user());
 
         return ['exists' => $items->filter(function ($item) use ($request) {
-                return $item->id == $request->product_id;
-            })->count() > 0];
+            return $item->id == $request->product_id;
+        })->count() > 0];
     }
 
 }
