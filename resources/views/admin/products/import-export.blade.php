51efd9ef1be528b7d0ce2104f5216002f4c5350c
Prem Neupane <bindas.prem.75@gmail.com>
2021-07-14 12:19:23 +0545

    export with image

--- resources/views/admin/products/import-export.blade.php
+++ resources/views/admin/products/import-export.blade.php
@@ -60,7 +60,7 @@
                                 <div class="card-body">
                                     <form action="{{ route('admin.products.import') }}" method="POST" enctype="multipart/form-data">
                                         @csrf
-                                        <input type="file" name="file" class="form-control">
+                                        <input type="file" name="file" class="form-control" required>
                                         <br>
                                         <button class="btn btn-success">Import Products Data</button>
                                         <a class="btn btn-warning" href="{{ route('admin.products.export') }}">Export Products Data</a>
