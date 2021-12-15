3de5f527955af9b07a2dc7e0607a11d2f26e2050
Hemanta Rijal <foreverhemanta221@gmail.com>
2021-12-15 22:54:51 +0545

    home page

--- resources/views/frontend/includes/banner.blade.php
+++ resources/views/frontend/includes/banner.blade.php
@@ -1,8 +1,27 @@
+
+@php
+    $mobile_responsive_banner = get_banner_by_position('mobile_responsive');
+@endphp
+
 <!-- Hero Section Begin -->
 <section class="hero-section">
     <div class="hero-items owl-carousel">
         @foreach(get_banner_by_position('desktop') as $index => $slide)
-            <div class="single-hero-items set-bg" data-setbg="{{map_storage_path_to_link($slide->image)}}"></div>
+            <picture>
+                <source media="(min-width: 460px)" srcset="{{map_storage_path_to_link($slide->image)}}">
+                  {{--check mobile responsive--}}
+                @if(!$mobile_responsive_banner->isEmpty())
+                    @if(isset($mobile_responsive_banner[$index]))
+                        <img src="{{map_storage_path_to_link($mobile_responsive_banner[$index]->image)}}?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
+                    @else
+                        <img src="{{map_storage_path_to_link($slide->image)}}?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940">
+
+                    @endif
+              @else
+                    <img src="{{map_storage_path_to_link($slide->image)}}">
+                @endif
+
+            </picture>
         @endforeach
     </div>
 </section>
