3e4a69fdd19af11dc95608ec0635067a7f0fcfd6
Hemanta Rijal <foreverhemanta221@gmail.com>
2021-12-29 00:30:39 +0545

    brand filter implementation

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Brand;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Modules\Brand\Services\BrandService;
 use Modules\Products\Services\ProductService;
 
@@ -15,8 +20,22 @@ class BrandController extends Controller
         $this->brandService = $brandService;
         $this->productService = $productService;
     }
-    public function getBrandsByProductSize(){
-        $brands = $this->brandService->getBrandWithProductCount();
-        return view('frontend.brands.brands')->with('brands',$brands);
+    public function getBrandsByProductSize(Request $request){
+        $name = $request->has('name')? $request->get('name'):null;
+        $brands = $this->brandService->getBrandWithProductCount($name);
+        $options = [
+            "path" => URL::to('brands'),
+            "pageName" => "page"
+        ];
+        return view('frontend.brands.brands')->with('brands', $this->paginate($brands,$perPage = 15, $page = null, $options));
+    }
+
+    public function paginate($items, $perPage = 15, $page = null, $options = [])
+    {
+        $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+
+        $items = $items instanceof Collection ? $items : Collection::make($items);
+
+        return new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
     }
 }
--- app/Modules/Brand/Contracts/BrandRepo.php
+++ app/Modules/Brand/Contracts/BrandRepo.php
@@ -10,6 +10,6 @@ interface BrandRepo
     public function create($data);
     public function update($id, $data);
     public function delete($id);
-    public function getBrandWithProductCount();
+    public function getBrandWithProductCount($name);
     public function findBy($column, $data);
 }
