007bac1620dcab277a1fffec142ad5256df20b41
Hemanta Rijal <foreverhemanta221@gmail.com>
2022-01-22 23:01:03 +0545

    session value reset

--- config/session.php
+++ config/session.php
@@ -148,7 +148,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', 'www.murarkey.com'),
+    'domain' => env('SESSION_DOMAIN', ''),
 
     /*
     |--------------------------------------------------------------------------
