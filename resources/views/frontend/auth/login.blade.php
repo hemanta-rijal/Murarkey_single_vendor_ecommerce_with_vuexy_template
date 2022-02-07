088bfebff0d03826e5a9436bc2783ccd074984a8
Hemanta Rijal <foreverhemanta221@gmail.com>
2022-02-08 00:24:17 +0545

    login with facebook disables

--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -58,12 +58,12 @@
                             </form>
                             <!-- social login -->
                             <div class="social-login">
-                                <a href="{{ url('/login/facebook') }}" class="btn btn-social btn-fb">
-                                    <span>
-                                        <i class="fa fa-facebook-square" aria-hidden="true"></i>
-                                    </span>
-                                    <p>Login with Facebook</p>
-                                </a>
+{{--                                <a href="{{ url('/login/facebook') }}" class="btn btn-social btn-fb">--}}
+{{--                                    <span>--}}
+{{--                                        <i class="fa fa-facebook-square" aria-hidden="true"></i>--}}
+{{--                                    </span>--}}
+{{--                                    <p>Login with Facebook</p>--}}
+{{--                                </a>--}}
                                 <a href="{{ url('/login/google') }}" class="btn btn-social">
                                     <span>
                                         <img src="{{ URL::asset('frontend/img/google-g-logo-web.png') }}" alt="">
