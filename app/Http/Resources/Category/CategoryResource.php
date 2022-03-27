6837353e3e6f849c4a4ef88af4066b2dc715fbb8
Hemanta Rijal <foreverhemanta221@gmail.com>
2022-03-27 20:51:25 +0545

    category image improved

--- app/Http/Resources/Category/CategoryResource.php
+++ app/Http/Resources/Category/CategoryResource.php
@@ -23,10 +23,10 @@ class CategoryResource extends JsonResource
             "_lft" => $this->_lft,
             "_rgt" => $this->_rgt,
             "productCount" => $this->product_count,
-            "iconImagePath" => $this->icon_path,
-            "iconUrl" => map_storage_path_to_link($this->icon_path),
-            "imagePath" => $this->image_path,
-            "imageUrl" => map_storage_path_to_link($this->image_url),
+            "icon_path" => $this->icon_path,
+            "icon_url" => map_storage_path_to_link($this->icon_path),
+            "image_path" => $this->image_path,
+            "image_url" => map_storage_path_to_link($this->image_url),
             "children" => CategoryResource::collection($this->child_category),
         ];
     }
