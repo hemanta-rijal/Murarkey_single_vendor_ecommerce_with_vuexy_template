fc917bb84f02e2ddcef4ed686bc772d43763b06d
Hemanta Rijal <foreverhemanta221@gmail.com>
2022-04-06 22:36:27 +0545

    service image gallery

--- app/Http/Resources/Service/ServiceResource.php
+++ app/Http/Resources/Service/ServiceResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Service;
 
+use App\Http\Resources\product\ImageResource;
 use App\Http\Resources\Review\ReviewResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -23,6 +24,7 @@ class ServiceResource extends JsonResource
             'icon_image' => resize_image_url($this->icon_image, '100X100'),
             // 'service_category' => new ServiceCategoryResource($this->serviceCategory),
             'featured_image' => resize_image_url($this->featured_image, '600X600'),
+            "images" => ImageResource::collection($this->images),
             'short_description' => $this->service_quote,
             'description' => $this->description,
             'price' => $this->price,
